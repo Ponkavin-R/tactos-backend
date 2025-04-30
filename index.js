706ba8a6c0dfbc7b97d89705f0e4c950793f6719
PonKavin <154010510+Ponkavin-R@users.noreply.github.com>
2025-04-30 18:25:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://tactos.in'], // adjust as needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
